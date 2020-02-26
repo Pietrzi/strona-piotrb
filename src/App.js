9149aef4a3925e3619be26d70b89966ca3e13d05
Pietrzi <pietrzi@op.pl>
2020-02-26 18:09:04 +0100

    prepared routing for page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import './App.css';
-import sample from './Surfer1.mp4'
+import {
+  HashRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Movie from './components/Movie';
+import MovieArt from './components/MovieArt';
+import Paint from './components/Paint';
+//import sample from './assets/Surfer1.mp4';
+
 
 function App() {
   return (
     <>
-    <header>
-      <video src={sample} autoPlay="true" loop="true"></video>
-    </header>
-    <div className="content">
-      <h1>Navbar</h1>
-      <p>content</p>
-      <p>footer</p>
-    </div>
+      <HashRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Main} />
+          <Route path='/movie' component={Movie} />
+          <Route path='/movieart' component={MovieArt} />
+          <Route path='/paint' component={Paint} />
+        </Switch>
+        <Footer />
+      </HashRouter>
     </>
   );
 }
