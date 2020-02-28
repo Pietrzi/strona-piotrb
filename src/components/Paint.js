537cc88475d75dd7c1578830a84151c40c6a9c98
Pietrzi <pietrzi@op.pl>
2020-02-28 08:44:46 +0100

    started carousel for paint section

--- src/components/Paint.js
+++ src/components/Paint.js
@@ -1,11 +1,58 @@
 import React from 'react';
+import Slide from './Slide';
+import firstPaint from "./assets/raz.jpg";
+import secondPaint from "./assets/dwa.jpg";
+import thirdPaint from "./assets/trzy.jpg";
 
-const Paint = () => {
-    return (
-        <div className="temp__box">
-            <h1>Paint</h1>
-        </div>
-    )
+
+class Paint extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          currentIndex: 0,
+          images: [firstPaint, secondPaint, thirdPaint]
+        };
+    }
+
+    prevSlide = () => {
+    const lastIndex = this.state.images.length - 1;
+    const resetIndex = this.state.currentIndex === 0;
+    const index = resetIndex ? lastIndex : this.state.currentIndex - 1;
+    this.setState({
+        currentIndex: index
+        });
+    };
+    nextSlide = () => {
+    const lastIndex = this.state.images.length - 1;
+    const resetIndex = this.state.currentIndex === lastIndex;
+    const index = resetIndex ? 0 : this.state.currentIndex + 1;
+    this.setState({
+        currentIndex: index
+        });
+    };
+    
+    render() {
+
+        const index = this.state.currentIndex;
+        let newImagesArray = this.state.images.slice(index, index + 3);
+        if (newImagesArray.length < 3) {
+        newImagesArray = newImagesArray.concat(
+            this.state.images.slice(0, 3 - newImagesArray.length)
+        );
+        }
+
+        return (
+            <div className="temp__box">
+                {newImagesArray.map((image, i) =>
+                this.state.currentIndex === i ? (
+                    <Slide key={i} url={image} alt="" />
+                ) : null
+                )}
+                <div>left</div>
+                <div>right</div>
+            </div>
+        )
+    }
 }
 
 export default Paint;
