327d72d48626d6d68851cc42c4967f3cb8459358
Pietrzi <pietrzi@op.pl>
2020-03-04 15:07:10 +0100

    video still not the size I want...

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import { DefaultPlayer as Video } from 'react-html5video';
-import 'react-html5video/dist/styles.css';
+import Reactplayer from './ReactPlayer';
 
 
 const Film = () => {
     return (
         <div className="film__container">
-            <div className="film__box">
-            <Video autoPlay loop muted
-                controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
-                poster="../assets/telko.png"
-                onCanPlayThrough={() => {
-                    // Do stuff
-                }}>
-                <source src="../assets/3333.mp4" type="video/webm" />
-                <track label="English" kind="subtitles" srcLang="en" src="http://source.vtt" default />
-            </Video>
-            </div>
+            <Reactplayer />
         </div>
     )
 }
---
+++ src/components/ReactPlayer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+//import VideoPlayer from 'react-simple-video-player';
+import video from '../assets/3333.mp4';
+
+class Reactplayer extends React.Component {
+
+    render() {
+        return (
+            <div className='player-wrapper'>
+                <ReactPlayer
+                className='react-player'
+                url={video}
+                width='60%'
+                height='60%'
+                controls={true}
+                />
+            </div>
+        )
+    }
+    
+}
+
+export default Reactplayer;
