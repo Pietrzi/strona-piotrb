a19abb5ff427091de8cf90b42f56f1443d3bb2ad
Pietrzi <pietrzi@op.pl>
2020-03-18 17:36:57 +0100

    learned new way to render component depend on screen size

--- src/components/BioContact.js
+++ src/components/BioContact.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import sample from '../assets/JUPI.mp4';
 
-const BioContact = () => {
-    return (
-        <video src={sample} autoPlay={true} loop={true} muted></video>
-    )
+class BioContact extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { matches: window.matchMedia("(min-width: 668px)").matches };
+      }
+
+    componentDidMount() {
+    const handler = e => this.setState({matches: e.matches});
+    window.matchMedia("(min-width: 668px)").addListener(handler);
+    }
+
+
+    render() {
+        return (
+            <div >
+                {this.state.matches && (<video src={sample} autoPlay={true} loop={true} muted></video>)}
+                {!this.state.matches && (<div className="pic__background"></div>)}
+            </div>
+            
+        )
+    }
 }
 
 export default BioContact;
