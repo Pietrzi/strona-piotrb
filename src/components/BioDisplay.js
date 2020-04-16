8b913a27972d9e4fd4b546a5b3548b3c45f7b67e
Pietrzi <pietrzi@op.pl>
2020-04-16 12:32:10 +0200

    new bio desktop

--- src/components/BioDisplay.js
+++ src/components/BioDisplay.js
@@ -5,10 +5,14 @@ const BioContact = () => {
     return (
         <div className="bio__container">
             <div className="bio__content__container">
-                <div className="bio__picture"></div>
-                <p className="bio__title">Ach gdyby tak błądzę</p>
-                <div className="bio__text__holder">
-                    <p>Oszczędności w portach na granicy światów widać gołym okiem, gołym uchem słychać niewykwalifikowany</p>
+                <div className="bio__left">
+                    <div className="bio__picture"></div>
+                </div>
+                <div className="bio__right">
+                    <p className="bio__title">Piotr Bartos</p>
+                    <div className="bio__text__holder">
+                        <p>Wrocławski reżyser, operator, grafik i artysta eksperymentujący z ruchomym obrazem. Laureat nagrody filmowej na Festiwalu Nowe Horyzonty za film dokumentalny "Chłopiec z Żabą". Wraz z Tomkiem Tryzną współtworzył foto prozę "Taniec w Skorupkach". Autor wielu filmów video-artowych i krótkometrażowych między innymi "Egzul" z nagrodą główną na festiwalu Shortcutz Amsterdam.  Współtwórca wrocławskiego festiwalu Kinomural. Aktualnie pochłonięty Czerwoną Księgą Carla Gustava Junga.</p>
+                    </div>
                 </div>
             </div>
         </div>
