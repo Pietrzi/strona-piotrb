c55607b87b502866c301a61cd58a736461577a87
Pietrzi <pietrzi@op.pl>
2020-04-27 18:41:02 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/strona-piotrb/precache-manifest.e89b606e96e3ae6c260c7d2f96dfb985.js"
+  "/strona-piotrb/precache-manifest.7b5c3ba5afedec864743f4f04a1c6e93.js"
 );
 
 self.addEventListener('message', (event) => {
